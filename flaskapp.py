a26ab001d1e379feac69bbce0e1dc9a5771b6d8a
Paul Broadbent <dbdkmezz@gmail.com>
2016-03-13 14:41:44 +0000

    added name search

--- flaskapp.py
+++ flaskapp.py
@@ -9,13 +9,25 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
+        return {'hello': 'world'}
+
+class heroes(Resource):
+    def get(self):
         conn = e.connect()
         query = conn.execute("SELECT name FROM Heroes")
         return {'Heroes': [i[0] for i in query.cursor.fetchall()]}
 
-#         return {'hello': 'world'}
+class hero_name(Resource):
+    def get(self, hero_id):
+        conn = e.connect()
+        query = conn.execute("SELECT name FROM Heroes WHERE Heroes._id = {}".format(hero_id))
+        return {'Name': [i[0] for i in query.cursor.fetchall()]}
+
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(heroes, '/heroes')
+api.add_resource(hero_name, '/hero/<string:hero_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+#    app.run(debug=True)
