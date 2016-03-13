ec441b4c81514c4598d03d0312ad45d1664b52f7
Paul Broadbent <dbdkmezz@gmail.com>
2016-03-13 14:36:11 +0000

    database

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+
+e = create_engine('sqlite:///app-root/data/advantages.db')
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        conn = e.connect()
+        query = conn.execute("SELECT name FROM Heroes")
+        return {'Heroes': [i[0] for i in query.cursor.fetchall()]}
+
+#         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
 
