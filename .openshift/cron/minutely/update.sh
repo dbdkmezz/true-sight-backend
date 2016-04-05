d95c2b76c8dffad352754cd643c082395de70a65
Paul Broadbent <dbdkmezz@gmail.com>
2016-04-05 17:10:42 +0100

    testing update

--- .openshift/cron/minutely/update.sh
+++ .openshift/cron/minutely/update.sh
@@ -2,5 +2,5 @@
 
 date >> ${OPENSHIFT_PYTHON_LOG_DIR}/update.log
 echo "test" >> ${OPENSHIFT_PYTHON_LOG_DIR}/update.log
-# python3 ~/app-root/repo/web-scraper/bin.py >> ${OPENSHIFT_PYTHON_LOG_DIR}/update.log
-# cp ~/app-root/repo/web-scraper/advantages.db ~/app-root/data/
+python3 ~/app-root/repo/web-scraper/bin.py >> ${OPENSHIFT_PYTHON_LOG_DIR}/update.log
+cp ~/app-root/repo/web-scraper/advantages.db ~/app-root/data/
