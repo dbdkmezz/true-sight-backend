1230cd66b93a595e38abe23b87d5c83d854f1d3a
Paul Broadbent <dbdkmezz@gmail.com>
2016-04-05 16:57:48 +0100

    temp moved to minuitely updates

--- .openshift/cron/daily/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-date >> ${OPENSHIFT_PHP_LOG_DIR}/update.log
-python3 ~/app-root/repo/web-scraper/bin.py >> ${OPENSHIFT_PYTHON_LOG_DIR}/update.log
-cp ~/app-root/repo/web-scraper/advantages.db ~/app-root/data/
