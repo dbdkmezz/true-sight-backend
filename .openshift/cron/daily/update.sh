5a8072e5e388bb4cad59a8ca8821a58b4699e00d
Paul Broadbent <dbdkmezz@gmail.com>
2016-04-05 16:48:11 +0100

    added update log

--- .openshift/cron/daily/update.sh
+++ .openshift/cron/daily/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 ~/app-root/repo/web-scraper/bin.py
+date >> ${OPENSHIFT_PHP_LOG_DIR}/update.log
+python3 ~/app-root/repo/web-scraper/bin.py >> ${OPENSHIFT_PHP_LOG_DIR}/update.log
 cp ~/app-root/repo/web-scraper/advantages.db ~/app-root/data/
