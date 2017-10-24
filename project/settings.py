df292162b04cc1233ac1d7371bc842cfeb257061
Paul Carver <dbdkmezz@gmail.com>
2017-10-24 08:06:20 +0100

    allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,15 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+from socket import gethostname
+ALLOWED_HOSTS = [
+    gethostname(), # For internal OpenShift load balancer security purposes.
+    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
+    #'example.com', # First DNS alias (set up in the app)
+    #'www.example.com', # Second DNS alias (set up in the app)
+]
+
 
 
 # Application definition
