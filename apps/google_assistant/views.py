b073f6db7416be4ab48f614e2338f0ac7513354e
Paul Carver <dbdkmezz@gmail.com>
2018-03-31 17:43:13 +0100

    comments

--- apps/google_assistant/views.py
+++ apps/google_assistant/views.py
@@ -19,7 +19,6 @@ good_response_logger = logging.getLogger('good_response')
 # # Pre reddit
 # test that my exception logger works (then remove that code)
 # test that I say the valve stuff differently (and speak isn't printed)
-# cooldown, damange type ... of ultimate (OR at least let them ask more about it)
 # ensure I'm happy with the logging
 # persona?
 # fix all the ablities which aren't loading properly
